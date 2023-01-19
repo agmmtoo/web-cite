3114e5f9038b214b7e30281b5fa956b810ec07de
ammo <ammo@noobuntu.lan>
2023-01-19 21:32:01 +0630

    vite ts init

---
+++ web-site-frontend/src/router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import App from './App'
+import About from './pages/About'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
+])
