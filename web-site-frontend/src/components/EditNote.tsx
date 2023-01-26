8e509d31c0e51e43da7e9b7a25ff017b18220c52
ammo <solidifyarmor@gmail.com>
2023-01-26 11:26:54 +0630

    save edited

--- web-site-frontend/src/components/EditNote.tsx
+++ web-site-frontend/src/components/EditNote.tsx
@@ -1,17 +1,20 @@
 import { useLoaderData, useNavigate } from 'react-router-dom'
 
 import NoteModal from './NoteModal'
+
+import { updateNote } from '../api/notes.api'
+
 import { Note } from '../types/notes.types'
 
 export default function EditNote() {
   const note = useLoaderData() as Note
   const navigate = useNavigate()
   const goBack = () => navigate('..')
-  const handleClose = () => {
-    goBack()
+  const handleClose = (note) => {
+    updateNote({ key: note.key, note }).then(console.log).finally(goBack)
   }
   return (
-    <NoteModal open={true} onClose={console.log} onSubmit={console.log} initialValues={note}>
+    <NoteModal open={true} onClose={handleClose} onSubmit={handleClose} initialValues={note} submitOnClose={true}>
       <h1>Edit Note</h1>
     </NoteModal>
   )
