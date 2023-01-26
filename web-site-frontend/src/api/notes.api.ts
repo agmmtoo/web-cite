71358e879b31d1fa8353fecce395c22b07b4086c
ammo <solidifyarmor@gmail.com>
2023-01-26 11:24:14 +0630

    api to update note

--- web-site-frontend/src/api/notes.api.ts
+++ web-site-frontend/src/api/notes.api.ts
@@ -1,13 +1,17 @@
 import supabase from './supabase.api'
 
 export const getNotes = async (userId: string) => {
-  const { error, data } = await supabase.from('notes').select().eq('user_id', userId)
+  const { error, data } = await supabase
+    .from('notes')
+    .select()
+    .eq('user_id', userId)
+    .order('created_at', { ascending: false })
   if (error) throw error
   return data
 }
 
-export const createNote = async ({ userId, title, content }) => {
-  const { error, data } = await supabase.from('notes').insert({ user_id: userId, title, content })
+export const createNote = async ({ userId, title, content, url }) => {
+  const { error, data } = await supabase.from('notes').insert({ user_id: userId, title, content, url })
   if (error) throw error
   return data
 }
@@ -19,3 +23,19 @@ export const getNote = async ({ getBy, value, signal }) => {
 }
 
 export const getNoteByKey = async (key: string, signal) => await getNote({ getBy: 'key', value: key, signal })
+
+export const updateNote = async ({ key, note }) => {
+  const { data, error } = await supabase
+    .from('notes')
+    .update({
+      title: note.title,
+      content: note.content,
+      url: note.url,
+    })
+    .eq('key', key)
+    .select()
+    .single()
+
+  if (error) throw error
+  return data
+}
