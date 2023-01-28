8de493185356a34c7b7dbfc60be742fb992630c5
ammo <solidifyarmor@gmail.com>
2023-01-28 20:46:03 +0630

    remove wrapper and use tilt hook instead

--- web-site-frontend/src/components/Note.tsx
+++ web-site-frontend/src/components/Note.tsx
@@ -1,23 +1,29 @@
 import { Link, useMatch } from 'react-router-dom'
-import TiltCard from './common/TiltCard'
+
 import classNames from '../utils/classNames'
+import useTilt from '../hooks/useTilt'
+
+import './Note.css'
 
 export default function Note({ note }) {
   const isActive = useMatch(`${note.key}`)
+  // "style" can be used directly
+  // but externall class is used in favour of element's cleaness
+  const { elemRef, style } = useTilt()
   return (
     <Link
+      // style={style}
+      ref={elemRef}
       to={`${note.key}`}
       className={classNames(
-        'tilt card leading-relaxed tracking-wide active:bg-green-100 hover:shadow-md break-words transition',
+        'tilt card leading-relaxed tracking-wide active:bg-green-100 hover:border-green-400 hover:shadow-md break-words transition',
         isActive ? 'bg-green-100' : '',
         largeContentClassname(note.content)
       )}
     >
-      <TiltCard>
-        <h4 className='font-medium'>{note.title}</h4>
-        <small>{note.url}</small>
-        <p className='overflow-hidden'>{largeContent(note.content)}</p>
-      </TiltCard>
+      <h4 className='font-medium'>{note.title}</h4>
+      <small>{note.url}</small>
+      <p className='overflow-hidden'>{largeContent(note.content)}</p>
     </Link>
   )
 }
