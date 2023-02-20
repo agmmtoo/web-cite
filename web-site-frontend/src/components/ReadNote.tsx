2889a6f232d2e49f4ad79a17035b2687981444fe
ammo <solidifyarmor@gmail.com>
2023-02-20 15:49:54 +0630

    readme docs

---
+++ web-site-frontend/src/components/ReadNote.tsx
@@ -0,0 +1,45 @@
+import { useLoaderData, useNavigate, Outlet, useMatch, Link } from 'react-router-dom'
+import { TrashIcon } from '@heroicons/react/24/outline'
+import { IconButton } from './common'
+
+import { Modal } from './common'
+
+import { updateNote, deleteNote } from '../api/notes.api'
+
+import { Note } from '../types/notes.types'
+
+export default function ReadNoe() {
+  const note = useLoaderData() as Note
+  const navigate = useNavigate()
+  const goBack = () => navigate('..')
+
+  const m = useMatch(String(note.key))
+  console.log(m)
+
+  // save form on close
+  // underlying NoteModal will handle if values changed or not
+  const handleClose = (note: Note) => {
+    // updateNote({ key: note.key, note }).finally(goBack)
+    goBack()
+  }
+
+  const onDelete = () => {
+    deleteNote(note.key).finally(goBack)
+  }
+
+  return (
+    <>
+        <Modal open={true} onClose={handleClose} initialValues={note} submitOnClose={true}>
+          {/* <IconButton type='button' onClick={onDelete}>
+        <TrashIcon className='hover:text-red-600' />
+      </IconButton> */}
+          <div className='w-full h-full flex flex-col justify-center items-center'>
+            <Link to='edit'>Edit</Link>
+          </div>
+          {
+            !m && <Outlet />
+          }
+        </Modal>
+    </>
+  )
+}
